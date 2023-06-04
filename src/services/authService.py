71f6c8e1c58fffbf61358afd47787246bb612ce9
DavidBarros <DavidB4rros@gmail.com>
2023-06-04 05:38:11 -0300

    userRoutes implementation

--- src/services/authService.py
+++ src/services/authService.py
@@ -1,18 +1,10 @@
-from werkzeug.security import check_password_hash, generate_password_hash
+from werkzeug.security import check_password_hash
 
 from ..extensions.database import mongo
-from ..model.user import User
 
 
 class AuthService:
 
-    @staticmethod        
-    def createUser(firstName, lastName, birthDate, email, password):
-        newUser = User(firstName, lastName,
-                       birthDate, email, generate_password_hash(password))
-        mongo.db.users.insert_one(newUser.to_dict())
-        return (newUser.to_dict())
-    
     @staticmethod
     def authenticateUser(email, password):
         user = mongo.db.users.find(email)
